e5bf74f393b8f5936127da0f67addb16f6ca69da
Markus Mathiasen <markusemathiasen@gmail.com>
2023-08-28 21:29:24 +0200

    Move files

--- chapter5/matrix_power/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-for line in sys.stdin:
-	if len(line) < 3: break
-	a, op, b = line.split()
-	a,b = int(a),int(b)
-	if op == "+": print((a+b)%10000)
-	if op == "*": print((a*b)%10000)
-	if op == "^": print(pow(a, b, 10000))
