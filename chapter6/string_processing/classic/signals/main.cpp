fffda9904e9a640385a6e91b2aa4fefaa2427bae
Markus Mathiasen <markusemathiasen@gmail.com>
2023-10-19 08:09:21 +0200

    signals accepted

---
+++ chapter6/string_processing/classic/signals/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(ll i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+typedef long long ll;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+template<class I> vi lis(const vector<I>& S) {
+	if (S.empty()) return {};
+	vi prev(sz(S));
+	typedef pair<I, ll> p;
+	vector<p> res;
+	rep(i,0,sz(S)) {
+		// change 0 -> i for longest non-decreasing subsequence
+		auto it = lower_bound(all(res), p{S[i], 0});
+		if (it == res.end()) res.emplace_back(), it = res.end()-1;
+		*it = {S[i], i};
+		prev[i] = it == res.begin() ? 0 : (it-1)->second;
+	}
+	ll L = sz(res), cur = res.back().second;
+	vi ans(L);
+	while (L--) ans[L] = cur, cur = prev[cur];
+	return ans;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	ll n; cin >> n;
+	vi A(n);
+	rep(i, 0, n) cin >> A[i];
+	printf("%lld\n", sz(lis(A)));
+}
