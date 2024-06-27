0ff383dee80470f6160b8674854e044b20669503
Markus Mathiasen <markusemathiasen@gmail.com>
2024-06-27 09:07:58 +0200

    repeated_substrings accepted

---
+++ chapter6/suffix_trie_tree_array/repeated_substrings/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(ll i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+typedef long long ll;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+struct SuffixArray {
+	vi sa, lcp;
+	SuffixArray(string& s, ll lim=256) { // or basic_string<ll>
+		ll n = sz(s) + 1, k = 0, a, b;
+		vi x(all(s)+1), y(n), ws(max(n, lim)), rank(n);
+		sa = lcp = y, iota(all(sa), 0);
+		for (ll j = 0, p = 0; p < n; j = max(1ll, j * 2), lim = p) {
+			p = j, iota(all(y), n - j);
+			rep(i,0,n) if (sa[i] >= j) y[p++] = sa[i] - j;
+			fill(all(ws), 0);
+			rep(i,0,n) ws[x[i]]++;
+			rep(i,1,lim) ws[i] += ws[i - 1];
+			for (ll i = n; i--;) sa[--ws[x[y[i]]]] = y[i];
+			swap(x, y), p = 1, x[sa[0]] = 0;
+			rep(i,1,n) a = sa[i - 1], b = sa[i], x[b] =
+				(y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
+		}
+		rep(i,1,n) rank[sa[i]] = i;
+		for (ll i = 0, j; i < n - 1; lcp[rank[i++]] = k)
+			for (k && k--, j = sa[rank[i] - 1];
+					s[i + k] == s[j + k]; k++);
+	}
+};
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	string s; cin >> s;
+	SuffixArray sa(s);
+	ll length = 0;
+	for (ll x : sa.lcp) length = max(length, x);
+	rep(i, 0, sz(sa.lcp)) {
+		if (sa.lcp[i] == length) {
+			printf("%s\n", s.substr(sa.sa[i], length).c_str());
+			return 0;
+		}
+	}
+}
