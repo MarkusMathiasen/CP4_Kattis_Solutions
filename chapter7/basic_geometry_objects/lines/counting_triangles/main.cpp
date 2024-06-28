034dfff02673267606304eae0ca6a2c47e317207
Markus Mathiasen <markusemathiasen@gmail.com>
2024-06-28 09:42:08 +0200

    counting_triangles accepted

---
+++ chapter7/basic_geometry_objects/lines/counting_triangles/main.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(ll i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+typedef long long ll;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+template <class T> ll sgn(T x) { return (x > 0) - (x < 0); }
+template<class T>
+struct Point {
+    typedef Point P;
+    T x, y;
+    explicit Point(T x=0, T y=0) : x(x), y(y) {}
+    bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+    bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+    P operator+(P p) const { return P(x+p.x, y+p.y); }
+    P operator-(P p) const { return P(x-p.x, y-p.y); }
+    P operator*(T d) const { return P(x*d, y*d); }
+    P operator/(T d) const { return P(x/d, y/d); }
+    T dot(P p) const { return x*p.x + y*p.y; }
+    T cross(P p) const { return x*p.y - y*p.x; }
+    T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+    T dist2() const { return x*x + y*y; }
+    double dist() const { return sqrt((double)dist2()); }
+    // angle to x-axis in interval [-pi, pi]
+    double angle() const { return atan2(y, x); }
+    P unit() const { return *this/dist(); } // makes dist()=1
+    P perp() const { return P(-y, x); } // rotates +90 degrees
+    P normal() const { return perp().unit(); }
+    // returns point rotated 'a' radians ccw around the origin
+    P rotate(double a) const {
+        return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+    friend ostream& operator<<(ostream& os, P p) {
+        return os << "(" << p.x << "," << p.y << ")"; }
+};
+template<class P> bool onSegment(P s, P e, P p) {
+    return p.cross(s, e) == 0 && (s - p).dot(e - p) <= 0;
+}
+template<class P> vector<P> segInter(P a, P b, P c, P d) {
+    auto oa = c.cross(d, a), ob = c.cross(d, b),
+         oc = a.cross(b, c), od = a.cross(b, d);
+    // Checks if intersection is single non-endpoint point.
+    if (sgn(oa) * sgn(ob) < 0 && sgn(oc) * sgn(od) < 0)
+        return {(a * ob - b * oa) / (ob - oa)};
+    set<P> s;
+    if (onSegment(c, d, a)) s.insert(a);
+    if (onSegment(c, d, b)) s.insert(b);
+    if (onSegment(a, b, c)) s.insert(c);
+    if (onSegment(a, b, d)) s.insert(d);
+    return {all(s)};
+}
+
+vector<pair<Point<long double>, Point<long double>>> segs;
+ll res = 0, n;
+
+ll read_real() {
+    ll a, b; scanf("%lld.%lld", &a, &b);
+    rep(i, to_string(b).size(), 6) b*=10;
+    return a*1000000+b;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    while ((cin >> n), n != 0) {
+        segs.clear();
+        res=0;
+        rep(i, 0, n) {
+            long double x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+            Point<long double> p1(x1, y1), p2(x2, y2);
+            segs.push_back({p1, p2});
+            rep(j, 0, i)
+                rep(k, j+1, i)
+                    if (!segInter(segs[j].first, segs[j].second, segs[k].first, segs[k].second).empty()
+                            && !segInter(segs[j].first, segs[j].second, segs[i].first, segs[i].second).empty()
+                            && !segInter(segs[k].first, segs[k].second, segs[i].first, segs[i].second).empty())
+                        res++;
+        }
+        printf("%lld\n", res);
+    }
+}
