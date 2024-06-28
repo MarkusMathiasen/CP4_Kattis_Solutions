c00a3051b7eaf19ba497f9040782d2ebf4eb6f50
Markus Mathiasen <markusemathiasen@gmail.com>
2024-06-28 12:07:41 +0200

    trojke accepted

---
+++ chapter7/basic_geometry_objects/lines/trojke/main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(ll i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+typedef long long ll;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+template<class T>
+struct Point {
+	typedef Point P;
+	T x, y;
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
+	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+	T dist2() const { return x*x + y*y; }
+	double dist() const { return sqrt((double)dist2()); }
+	// angle to x-axis in interval [-pi, pi]
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this/dist(); } // makes dist()=1
+	P perp() const { return P(-y, x); } // rotates +90 degrees
+	P normal() const { return perp().unit(); }
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+	friend ostream& operator<<(ostream& os, P p) {
+		return os << "(" << p.x << "," << p.y << ")"; }
+};
+template<class P>
+bool onLine(const P& a, const P& b, const P& p) {
+	return (b-a).cross(p-a)==0ll;
+}
+
+ll n;
+vector<Point<ll>> A;
+vector<string> grid;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> n;
+	grid.assign(n, "");
+	rep(i, 0, n) {
+		cin >> grid[i];
+		rep(j, 0, n) if (grid[i][j] != '.') A.push_back(Point<ll>(i, j));
+	}
+	ll res = 0;
+	rep(i, 0, sz(A)) rep(j, i+1, sz(A)) rep(k, j+1, sz(A)) res += onLine(A[i], A[j], A[k]);
+	printf("%lld\n", res);
+}
