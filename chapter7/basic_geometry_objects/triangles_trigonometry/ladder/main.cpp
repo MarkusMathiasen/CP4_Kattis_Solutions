f977286baf22507ee77ca91efc4fbc7231bf3f57
Markus Mathiasen <markusemathiasen@gmail.com>
2024-07-01 11:30:19 +0200

    ladder accepted

---
+++ chapter7/basic_geometry_objects/triangles_trigonometry/ladder/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    //cin.exceptions(cin.failbit);
+    int h, v; scanf("%d%d", &h, &v);
+    printf("%d\n", (int)ceil(h/sin(v/180.0*M_PI)));
+}
