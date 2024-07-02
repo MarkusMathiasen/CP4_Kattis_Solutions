f15dedcdc9a8d0da7574c9fcb75cac5e82ea34e9
Markus Mathiasen <markusemathiasen@gmail.com>
2024-07-02 17:18:32 +0200

    point_in_polygon accepted

---
+++ chapter7/polygon/polygon_harder/point_in_polygon/main.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(ll i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+typedef long long ll;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+template<class T>
+struct Point {
+	typedef Point P;
+	T x, y;
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
+	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+	T dist2() const { return x*x + y*y; }
+	double dist() const { return sqrt((double)dist2()); }
+	// angle to x-axis in interval [-pi, pi]
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this/dist(); } // makes dist()=1
+	P perp() const { return P(-y, x); } // rotates +90 degrees
+	P normal() const { return perp().unit(); }
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+	friend ostream& operator<<(ostream& os, P p) {
+		return os << "(" << p.x << "," << p.y << ")"; }
+};
+template<class P> bool onSegment(P s, P e, P p) {
+	return p.cross(s, e) == 0 && (s - p).dot(e - p) <= 0;
+}
+template<class P>
+bool inPolygon(vector<P> &p, P a, bool strict = true) {
+	ll cnt = 0, n = sz(p);
+	rep(i,0,n) {
+		P q = p[(i + 1) % n];
+		if (onSegment(p[i], q, a)) return !strict;
+		//or: if (segDist(p[i], q, a) <= eps) return !strict;
+		cnt ^= ((a.y<p[i].y) - (a.y<q.y)) * a.cross(p[i], q) > 0;
+	}
+	return cnt;
+}
+
+ll n, m;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	while ((cin >> n), n > 0) {
+		vector<Point<ll>> poly(n);
+		rep(i, 0, n) cin >> poly[i].x >> poly[i].y;
+		cin >> m;
+		while (m--) {
+			Point<ll> p; cin >> p.x >> p.y;
+			bool s = inPolygon(poly, p, true);
+			bool not_s = inPolygon(poly, p, false);
+			if (s) printf("in\n");
+			else if (not_s) printf("on\n");
+			else printf("out\n");
+		}
+	}
+
+}
