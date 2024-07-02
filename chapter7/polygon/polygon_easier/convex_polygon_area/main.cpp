79c6ec9f1b3614f03c4979340c1350fce9f953d4
Markus Mathiasen <markusemathiasen@gmail.com>
2024-07-02 13:35:42 +0200

    convex_polygon_area accepted

---
+++ chapter7/polygon/polygon_easier/convex_polygon_area/main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+template<class T>
+struct Point {
+    typedef Point P;
+    T x, y;
+    explicit Point(T x=0, T y=0) : x(x), y(y) {}
+    bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+    bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+    P operator+(P p) const { return P(x+p.x, y+p.y); }
+    P operator-(P p) const { return P(x-p.x, y-p.y); }
+    P operator*(T d) const { return P(x*d, y*d); }
+    P operator/(T d) const { return P(x/d, y/d); }
+    T dot(P p) const { return x*p.x + y*p.y; }
+    T cross(P p) const { return x*p.y - y*p.x; }
+    T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+    T dist2() const { return x*x + y*y; }
+    double dist() const { return sqrt((double)dist2()); }
+    // angle to x-axis in interval [-pi, pi]
+    double angle() const { return atan2(y, x); }
+    P unit() const { return *this/dist(); } // makes dist()=1
+    P perp() const { return P(-y, x); } // rotates +90 degrees
+    P normal() const { return perp().unit(); }
+    // returns point rotated 'a' radians ccw around the origin
+    P rotate(double a) const {
+        return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+    friend ostream& operator<<(ostream& os, P p) {
+        return os << "(" << p.x << "," << p.y << ")"; }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    //cin.exceptions(cin.failbit);
+    int n; scanf("%d", &n);
+    while (n--) {
+        int m; scanf("%d", &m);
+        vector<Point<int>> points;
+        rep(i, 0, m) {
+            int x, y; scanf("%d%d", &x, &y);
+            points.push_back(Point<int>(x, y));
+        }
+        double area = 0;
+        rep(i, 1, m-1) {
+            area += (points[0].cross(points[i], points[i+1]))/2.0;
+        }
+        printf("%lf\n", area);
+    }
+}
