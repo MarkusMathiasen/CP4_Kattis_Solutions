da061a8fde63f14ec491ce62cef3cc539b3f5355
Markus Mathiasen <markusemathiasen@gmail.com>
2024-07-02 20:12:18 +0200

    skyline accepted

---
+++ chapter7/polygon/polygon_harder/skyline/main.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(ll i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+typedef long long ll;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+template <class T> ll sgn(T x) { return (x > 0) - (x < 0); }
+template<class T>
+struct Point {
+	typedef Point P;
+	T x, y;
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
+	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+	T dist2() const { return x*x + y*y; }
+	double dist() const { return sqrt((double)dist2()); }
+	// angle to x-axis in interval [-pi, pi]
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this/dist(); } // makes dist()=1
+	P perp() const { return P(-y, x); } // rotates +90 degrees
+	P normal() const { return perp().unit(); }
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+	friend ostream& operator<<(ostream& os, P p) {
+		return os << "(" << p.x << "," << p.y << ")"; }
+};
+template<class T>
+T polygonArea2(vector<Point<T>>& v) {
+	T a = v.back().cross(v[0]);
+	rep(i,0,sz(v)-1) a += v[i].cross(v[i+1]);
+	return a;
+}
+template<class P>
+ll sideOf(P s, P e, P p) { return sgn(s.cross(e, p)); }
+
+template<class P>
+ll sideOf(const P& s, const P& e, const P& p, double eps) {
+	auto a = (e-s).cross(p-s);
+	double l = (e-s).dist()*eps;
+	return (a > l) - (a < -l);
+}
+
+typedef Point<double> P;
+double rat(P a, P b) { return sgn(b.x) ? a.x/b.x : a.y/b.y; }
+double polyUnion(vector<vector<P>>& poly) {
+	double ret = 0;
+	rep(i,0,sz(poly)) rep(v,0,sz(poly[i])) {
+		P A = poly[i][v], B = poly[i][(v + 1) % sz(poly[i])];
+		vector<pair<double, ll>> segs = {{0, 0}, {1, 0}};
+		rep(j,0,sz(poly)) if (i != j) {
+			rep(u,0,sz(poly[j])) {
+				P C = poly[j][u], D = poly[j][(u + 1) % sz(poly[j])];
+				ll sc = sideOf(A, B, C), sd = sideOf(A, B, D);
+				if (sc != sd) {
+					double sa = C.cross(D, A), sb = C.cross(D, B);
+					if (min(sc, sd) < 0)
+						segs.emplace_back(sa / (sa - sb), sgn(sc - sd));
+				} else if (!sc && !sd && j<i && sgn((B-A).dot(D-C))>0){
+					segs.emplace_back(rat(C - A, B - A), 1);
+					segs.emplace_back(rat(D - A, B - A), -1);
+				}
+			}
+		}
+		sort(all(segs));
+		for (auto& s : segs) s.first = min(max(s.first, 0.0), 1.0);
+		double sum = 0;
+		ll cnt = segs[0].second;
+		rep(j,1,sz(segs)) {
+			if (!cnt) sum += segs[j].first - segs[j - 1].first;
+			cnt += segs[j].second;
+		}
+		ret += A.cross(B) * sum;
+	}
+	return ret / 2;
+}
+
+ll n;
+vector<vector<P>> A;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> n;
+	double prev_area = 0;
+	rep(i, 0, n) {
+		double x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+		A.push_back({P(x1, 0), P(x2, 0), P(x2, y2), P(x1, y1)});
+		double my_area = polygonArea2(A.back())/2;
+		double total_area = polyUnion(A);
+		printf("%.8lf\n", (total_area-prev_area)/my_area);
+		prev_area = total_area;
+	}
+}
