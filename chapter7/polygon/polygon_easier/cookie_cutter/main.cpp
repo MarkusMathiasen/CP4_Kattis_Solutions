ea4dfa729c81cfdf3a7dc535f5afd61b0077a49e
Markus Mathiasen <markusemathiasen@gmail.com>
2024-07-02 15:09:15 +0200

    cookie_cutter accepted

---
+++ chapter7/polygon/polygon_easier/cookie_cutter/main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(ll i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+typedef long long ll;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+template<class T>
+struct Point {
+	typedef Point P;
+	T x, y;
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
+	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+	T dist2() const { return x*x + y*y; }
+	double dist() const { return sqrt((double)dist2()); }
+	// angle to x-axis in interval [-pi, pi]
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this/dist(); } // makes dist()=1
+	P perp() const { return P(-y, x); } // rotates +90 degrees
+	P normal() const { return perp().unit(); }
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+	friend ostream& operator<<(ostream& os, P p) {
+		return os << "(" << p.x << "," << p.y << ")"; }
+};
+template<class T>
+T polygonArea2(vector<Point<T>>& v) {
+	T a = v.back().cross(v[0]);
+	rep(i,0,sz(v)-1) a += v[i].cross(v[i+1]);
+	return a;
+}
+
+ll n;
+vector<Point<double>> A;
+double area;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> n;
+	A.resize(n);
+	rep(i, 0, n) cin >> A[i].x >> A[i].y;
+	cin >> area;
+	double area_before = polygonArea2(A)/2.;
+	double sq_ratio = area/area_before;
+	double ratio = sqrt(sq_ratio);
+	for (Point<double>& p : A) p = p*ratio;
+	double min_x = A[0].x, min_y = A[0].y;
+	rep(i, 1, n) min_x = min(min_x, A[i].x), min_y = min(min_y, A[i].y);
+	for (Point<double>& p : A) p.x -= min_x, p.y -= min_y;
+	for (Point<double> p : A) printf("%lf %lf\n", p.x, p.y);
+}
