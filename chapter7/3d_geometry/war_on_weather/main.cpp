bbd3c0c7123a4fbcdac0ee15692c81f5f9628ffb
Markus Mathiasen <markusemathiasen@gmail.com>
2024-07-04 12:10:57 +0200

    war_on_weather accepted

---
+++ chapter7/3d_geometry/war_on_weather/main.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(ll i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+typedef long long ll;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+
+template<class T> struct Point3D {
+	typedef Point3D P;
+	typedef const P& R;
+	T x, y, z;
+	explicit Point3D(T x=0, T y=0, T z=0) : x(x), y(y), z(z) {}
+	bool operator<(R p) const {
+		return tie(x, y, z) < tie(p.x, p.y, p.z); }
+	bool operator==(R p) const {
+		return tie(x, y, z) == tie(p.x, p.y, p.z); }
+	P operator+(R p) const { return P(x+p.x, y+p.y, z+p.z); }
+	P operator-(R p) const { return P(x-p.x, y-p.y, z-p.z); }
+	P operator*(T d) const { return P(x*d, y*d, z*d); }
+	P operator/(T d) const { return P(x/d, y/d, z/d); }
+	T dot(R p) const { return x*p.x + y*p.y + z*p.z; }
+	P cross(R p) const {
+		return P(y*p.z - z*p.y, z*p.x - x*p.z, x*p.y - y*p.x);
+	}
+	T dist2() const { return x*x + y*y + z*z; }
+	double dist() const { return sqrt((double)dist2()); }
+	//Azimuthal angle (longitude) to x-axis in interval [-pi, pi]
+	double phi() const { return atan2(y, x); } 
+	//Zenith angle (latitude) to the z-axis in interval [0, pi]
+	double theta() const { return atan2(sqrt(x*x+y*y),z); }
+	P unit() const { return *this/(T)dist(); } //makes dist()=1
+	//returns unit vector normal to *this and p
+	P normal(P p) const { return cross(p).unit(); }
+	//returns point rotated 'angle' radians ccw around axis
+	P rotate(double angle, P axis) const {
+		double s = sin(angle), c = cos(angle); P u = axis.unit();
+		return u*dot(u)*(1-c) + (*this)*c - cross(u)*s;
+	}
+};
+
+ll k, m;
+vector<Point3D<double>> K, M;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	while ((cin >> k >> m), k+m>0) {
+		K.resize(k);
+		ll res = 0;
+		rep(i, 0, k) cin >> K[i].x >> K[i].y >> K[i].z;
+		rep(i, 0, m) {
+			Point3D<double> p;
+			cin >> p.x >> p.y >> p.z;
+			bool seen = false;
+			rep(j, 0, k) seen |= p.dot(K[j]-p) >= 0;
+			res += seen;
+		}
+		printf("%lld\n", res);
+	}
+}
